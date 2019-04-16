ca74f1b769bd4b9db29b0b02cee5737ab0364dfb
anson <anson@zoeker.cn>
2019-04-16 12:15:54 +0800

    update

--- Classes/QiniuUploader.m
+++ Classes/QiniuUploader.m
@@ -8,7 +8,7 @@
 
 #import "QiniuUploader.h"
 
-#define kQiniuUploadURL @"up-z2.qiniup.com"
+#define kQiniuUploadURL @"https//up-z2.qiniup.com"
 #define kQiniuTaskKey @"qiniuTaskKey"
 
 typedef void (^UploadOneFileSucceededHandler)(NSInteger index, NSDictionary * _Nonnull info);
